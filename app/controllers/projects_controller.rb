c3038b85090bc18f268d79ede85aff23f0a8e423
Andres Gualde <andres@gualde.com>
2022-03-01 12:51:25 +0100

    project controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.user_id = current_user
+    if @project.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project = Project.update(project_params)
+    if @project.save
+      redirect_to #####project_show#####
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :location, :category_id)
+  end
+end
