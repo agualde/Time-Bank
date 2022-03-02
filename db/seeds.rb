525ee5c4a333a7739f6492b5b0b80323c89e9f6b
Kathleen Thorpe <kathleenthorpe64@gmail.com>
2022-03-01 17:55:32 -0800

    added array for catagorys

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 User.destroy_all
 Project.destroy_all
 puts "DB cleaned!"
-# user = User.create!(email: "dummy@gmail.com", password: "123456")
 
-# guitar-photos = [
-#   'https://images.unsplash.com/photo-1525201548942-d8732f6617a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3VpdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-#   'https://images.unsplash.com/photo-1558098329-a11cff621064?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3VpdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-# ]
+project_photos = [
+  'https://images.unsplash.com/photo-1525201548942-d8732f6617a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3VpdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+  'https://images.unsplash.com/photo-1558098329-a11cff621064?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3VpdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+  'https://images.unsplash.com/photo-1594643357134-1baf950059e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aG9iYmllc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+  'https://images.unsplash.com/photo-1598815272841-4e85a77e0a67?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aG9iYmllc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+  'https://images.unsplash.com/photo-1596364725424-7673f05f64b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGhvYmJpZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+  'https://images.unsplash.com/photo-1584992236310-6edddc08acff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvYmJpZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+  'https://images.unsplash.com/photo-1610558128766-64f6b95ba135?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhvYmJpZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+  'https://images.unsplash.com/photo-1621781727164-8121aec35c88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGhvYmJpZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+]
 
 avatars = [
   'https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
@@ -42,24 +39,25 @@ end
 puts "user #{User.count} created"
 
 10.times do
-  project =  Project.create!(
+  project = Project.create!(
     title: Faker::Lorem.paragraph(sentence_count: 1),
     description: Faker::Lorem.paragraph(sentence_count: 15),
     location: Faker::Address.city,
     category_id: 1,
     user_id: User.all.sample.id
   )
-  file1 = URI.open(avatars.sample)
-  file2 = URI.open(avatars.sample)
-  file3 = URI.open(avatars.sample)
-  file4 = URI.open(avatars.sample)
-  file5 = URI.open(avatars.sample)
+  file1 = URI.open(project_photos.sample)
+  file2 = URI.open(project_photos.sample)
+  file3 = URI.open(project_photos.sample)
+  file4 = URI.open(project_photos.sample)
+  file5 = URI.open(project_photos.sample)
 
   project.photos.attach([{ io: file1, filename: project.title, content_type: 'image/jpeg' },
     { io: file2, filename: project.title, content_type: 'image/jpeg' },
     { io: file3, filename: project.title, content_type: 'image/jpeg' },
     { io: file4, filename: project.title, content_type: 'image/jpeg' },
-    { io: file5, filename: project.title, content_type: 'image/jpeg' }])
+    { io: file5, filename: project.title, content_type: 'image/jpeg' }]
+  )
 end
 
 puts "projects #{Project.count} created"
