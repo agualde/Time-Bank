e7404bd1be07f0b8a01a0162a806d90daea78eee
Guillem Roma <guillemroma.lopez@gmail.com>
2022-03-02 15:54:58 +0100

    project index progress

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,9 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+    @current_day = Time.now.day
+    @current_month = Time.now.month
+    @current_year = Time.now.year
   end
 
   def new
