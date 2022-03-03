c94bb06a4473f084bc9dd4cd83c522688965e070
Andres Gualde <andres@gualde.com>
2022-03-03 11:29:10 +0100

    Users index btn

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
   post '/project/:project_id/projectfavorites', to: 'favorites#createfavorite', as: :project_favorite
   delete '/projectfavorite/:id', to: 'favorites#destroyfavorite', as: :project_favorite_delete
   delete '/bookings/:id', to: 'bookings#destroy', as: :booking
