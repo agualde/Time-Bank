ac33d989e2841d48aa30b6e4d4147be8c5af24b5
Guillem Roma <guillemroma.lopez@gmail.com>
2022-03-07 09:03:00 +0100

    fixed

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,9 @@ class Review < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  
+  belongs_to :user
+  has_one :user_review, dependent: :destroy
+
   validates :title, presence: true
   validates :content, presence: true
   validates :rating, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,16 @@ class User < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
   has_many :user_reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   has_many :favorite_projects, through: :favorites, source: :project
   has_many :booked_projects, through: :bookings, source: :project
-  has_many :reviews, through: :user_reviews, dependent: :destroy
 
   has_many :user_skills, dependent: :destroy
   has_many :skills, through: :user_skills
+
 
   has_one_attached :photo
 
