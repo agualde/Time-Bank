258a8651b41a6ca65a870fd785ef4ae04a8cd852
Andres Gualde <andres@gualde.com>
2022-03-07 13:00:40 +0100

    basic layout implementation of chat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :get_chats
+
+  private
+
+  def get_chats
+    @chatrooms = Chatroom.where(sender_id: current_user).or(Chatroom.where(receiver_id: current_user))
+  end
 end
