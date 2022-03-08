c7efbf9692be632ba95a6039b7500f2e88555907
Guillem Roma <guillemroma.lopez@gmail.com>
2022-03-08 15:50:28 +0100

    filter refactored

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,61 +3,76 @@ class ProjectsController < ApplicationController
 
   def index
     if params[:query].present?
+
+      #we pass queries in the homepage; this condition is specific for everytime we pass a query
+
       @projects = Project.global_search(params[:query])
       @user_favorites = Favorite.where(user_id: current_user)
+
     elsif params[:query] == "" || (params.keys == ["controller", "action"]) && (params[:controller] == "projects" && params[:action] == "index") || (params[:location] == "" && params[:category] == "" && params[:favorite] == "" && params[:collaborator] == "" && params[:controller] == "projects" && params[:action] == "index")
+
+      #we have grouped the different URLs with no content inside; in that case we want to return all the projects since there has not been any filter applied
+
       @projects = Project.all
       @user_favorites = Favorite.where(user_id: current_user)
     else
-      @projects = Project.where(location: params[:location]).or(Project.where(category_id: Category.all.where(name: params[:category]).ids.first))
-      @user_favorites = Favorite.where(user_id: current_user)
-      if params[:favorite] == "" && params[:collaborator] == ""
-        @projects
-        @user_favorites = Favorite.where(user_id: current_user)
-      elsif params[:favorite] != ""
-        Project.all.each do |project|
-          @projects = @projects.to_ary
-          if project.favorites.count == params[:favorite].to_i
-            @projects << project
+
+      #when we pass information in any of the parameters throught the project index filter we activate the filter
+      #in case there is one or more params without values we select all the projects by default, otherwise we select the values passed
+
+      @project_location = (params[:location].blank?) ? Project.all : Project.where(location: params[:location])
+      @project_category = (params[:category].blank?) ? Project.all : Project.where(category_id: Category.all.where(name: params[:category]).ids.first)
+      @project_favorites = (params[:favorite].blank?) ? Project.all : Project.joins(:favorites).group("projects.id").having("COUNT (*) >= #{params[:favorite]}")
+      @project_collaborators = Project.all
+
+      @projects = []
+      @approved_bookings = 0
+
+      #the filter will select all the projects that share the values defined in the filter (no value == all values)
+      #it will check what projects are in the 4 different variables predefined before
+      #in case there is a project that is shared across the 4 variables, it will display it
+
+      @project_location.each do |project|
+        if @project_category.include?(project) && @project_favorites.include?(project)
+          project.bookings.each do |booking|
+            @approved_bookings += 1 if booking.status == "Approved"
           end
+          @projects << project if @approved_bookings == params[:collaborator].to_i
+          @approved_bookings = 0
         end
-        @user_favorites = Favorite.where(user_id: current_user)
-      elsif params[:collaborator] != ""
-        Project.all.each do |project|
-          @projects = @projects.to_ary
-          if project.bookings.count == params[:collaborator].to_i
-            @projects << project
-          end
-        end
-        @user_favorites = Favorite.where(user_id: current_user)
-      else
-        @user_favorites = Favorite.where(user_id: current_user).order(created_at: :asc)
-        @projects = Project.all.order(created_at: :asc)
       end
+      @user_favorites = Favorite.where(user_id: current_user)
     end
 
-    @form_projects_locations = Project.all
+    #we create the filter options as follows
+    #the idea is that we need to get all the different options available, per filter
+    #in case there are duplicates, we need to earse them
+    #finally, we need to sort the items in each array
+
     @locations = []
-    @form_projects_locations.each do |project|
+    Project.all.each do |project|
       @locations << project.location
     end
+    @locations = @locations.uniq.sort
 
     @categories = []
     Category.all.each do |category|
       @categories << category.name
     end
+    @categories = @categories.uniq.sort
 
-    @form_projects_favorites = Project.all
     @favorites = []
-    @form_projects_favorites.each do |project|
+    Project.all.each do |project|
       @favorites << project.favorites.count
     end
+    @favorites = @favorites.uniq.sort
 
-    @form_projects_collaborators = Project.all
     @collaborators = []
-    @form_projects_collaborators.each do |project|
+    Project.all.each do |project|
       @collaborators << project.bookings.where(status: "Approved").count
     end
+    @collaborators = @collaborators.uniq.sort
+
   end
 
   def show
