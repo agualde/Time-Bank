33bff5fa0ce3a33462aea86efeddf652aa923d76
Kathleen Thorpe <kathleenthorpe64@gmail.com>
2022-03-07 16:58:13 -0800

    changed issues with javascript

--- app/javascript/plugins/init_sortable.js
+++ app/javascript/plugins/init_sortable.js
@@ -1,12 +1,15 @@
 import Sortable from "sortablejs"
 
-const favs = document.querySelector("#favorites-dashboard")
+
 
 const initSortable = () => {
+  const favs = document.querySelector("#favorites-dashboard")
+  if (favs) {
   Sortable.create(favs, {
     ghostClass: "ghost",
     animation: 150,
   })
+  }
 }
 
 export { initSortable }
