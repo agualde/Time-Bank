bcfea602556d114e7faab97508519109d6197f02
Andres Gualde <andres@gualde.com>
2022-03-08 15:16:49 +0100

    minor improvements

--- app/javascript/controllers/replace_controller.js
+++ app/javascript/controllers/replace_controller.js
@@ -19,13 +19,16 @@ export default class extends Controller {
   render(event) {
     event.preventDefault()
     console.log(event.target)
+    
     fetch(event.target.href, {
       headers: { "Accept": "text/plain" }
     })
     .then((res) => res.text())
     .then((data) => {
       this.outputTarget.innerHTML = data
-      console.log(data)
+      var objDiv = document.getElementById("messages");
+      objDiv.scrollTop = objDiv.scrollHeight;
+      console.log(objDiv)
     })
   }
 }
