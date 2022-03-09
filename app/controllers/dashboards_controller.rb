a850c9136eca10f8991929e09981d4ac0e9fcc13
Guillem Roma <guillemroma.lopez@gmail.com>
2022-03-09 16:50:49 +0100

    dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,5 +4,8 @@ class DashboardsController < ApplicationController
     @projects = Project.where(user_id: @user)
     @bookings = Booking.where(user_id: @user)
     @favorites = Favorite.where(user_id: @user)
+    @ratings = UserReview.where(user_id: @user.id).map do |review|
+      review.review.rating
+    end
   end
 end
